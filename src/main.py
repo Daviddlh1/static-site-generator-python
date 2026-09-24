fa641018f2993b7e17f58df2b2d6327bbfb83d15
david De la Hoz <dricardelo@gmail.com>
2026-09-24 10:46:22 -0500

    m

--- src/main.py
+++ src/main.py
@@ -2,12 +2,26 @@ import os
 import shutil
 
 def main():
-    public_directory_path = os.path.abspath("../public")
-    static_directory_path = os.path.abspath("../static")
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    public_directory_path = os.path.join(project_root, "public")
+    static_directory_path = os.path.join(project_root, "static")
     
     if os.path.exists(public_directory_path):
         shutil.rmtree(public_directory_path)
-    if os.path.exists(static_directory_path):
-        shutil.rmtree(static_directory_path)
+    # if os.path.exists(static_directory_path):
+    #     shutil.rmtree(static_directory_path)
+
+    os.makedirs(public_directory_path)
+    # os.makedirs(static_directory_path)
+    
+    files_to_copy = os.listdir(static_directory_path)
+    print(files_to_copy)
+    for f in files_to_copy:
+        if "png" in f:
+            os.mkdir(f"{public_directory_path}/images")
+            shutil.copy(os.path.join(static_directory_path, f),os.path.join(public_directory_path, "images"))
+        else:
+            shutil.copy(os.path.join(static_directory_path, f), os.path.join(public_directory_path))
+    
     
 main()
